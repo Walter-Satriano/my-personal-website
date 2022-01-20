080b9f272bf3048566b068c93e352b36878be4b2
Walter <walter@quomi.it>
2022-01-20 16:58:01 +0100

    imported i18n plugin in main.js file

--- src/main.js
+++ src/main.js
@@ -5,11 +5,13 @@ import store from "./store";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
+import i18n from "./i18n.js";
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
+  i18n,
   render: (h) => h(App),
 }).$mount("#app");
