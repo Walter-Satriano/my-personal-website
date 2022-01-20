1189592b60dfd3886979477699cf3fd143ddabf7
Walter <walter@quomi.it>
2022-01-20 11:36:03 +0100

    imported bootstrap 5 and bootstrap icons

--- src/main.js
+++ src/main.js
@@ -2,9 +2,9 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap-icons/font/bootstrap-icons.css';
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 Vue.config.productionTip = false;
 
