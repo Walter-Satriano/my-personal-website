19293b13b1f528dd52e663d9072a5a055ac1b359
Walter <walter@quomi.it>
2022-01-20 11:37:02 +0100

    created basic navbar visualization for mobile and tablet device

---
+++ src/components/NavbarMobile.vue
@@ -0,0 +1,15 @@
+<template>
+  <nav class="d-block d-lg-none bg-dark py-3 fixed-bottom text-center">
+    <h2 class="text-white">THIS IS NAVBAR MOBILE</h2>
+  </nav>
+</template>
+
+<script>
+  export default {
+    name: "MobileDesktop",
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
