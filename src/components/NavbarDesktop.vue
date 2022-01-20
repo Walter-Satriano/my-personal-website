500e87a11b9554c1bf5f17f0f2da1ab095358dbd
Walter <walter@quomi.it>
2022-01-20 11:36:46 +0100

    created basic navbar visualization for desktop device

---
+++ src/components/NavbarDesktop.vue
@@ -0,0 +1,15 @@
+<template>
+  <nav class="d-none d-lg-block bg-dark py-3 fixed-top text-center">
+    <h2 class="text-white">THIS IS NAVBAR DESKTOP</h2>
+  </nav>
+</template>
+
+<script>
+  export default {
+    name: "NavbarDesktop",
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
