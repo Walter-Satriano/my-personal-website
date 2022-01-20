be976e8ae4619783955f0609f0d55b3df1ddb7f0
Walter <walter@quomi.it>
2022-01-20 11:37:42 +0100

    imported components in app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,28 @@
 <template>
   <div id="app">
 
+    <NavbarDesktop />
+    <NavbarMobile />
+    <Header />
+
   </div>
 </template>
+
+<script>
+  import NavbarDesktop from "./components/NavbarDesktop.vue";
+  import NavbarMobile from "./components/NavbarMobile.vue";
+  import Header from "./components/Header.vue";
+
+  export default {
+    name: "App",
+    components: {
+      NavbarDesktop,
+      NavbarMobile,
+      Header
+    },
+  }
+</script>
+
 
 <style lang="scss" scoped>
 
