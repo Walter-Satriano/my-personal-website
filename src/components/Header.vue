8a49679a346b98064f9265f315d5327f26f502e6
Walter <walter@quomi.it>
2022-01-20 11:37:22 +0100

    created a basic header

---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container-fluid">
+    <div class="row justify-content-center align-items-center">
+      <div class="col-5">
+        <img src="../assets/io.jpg" alt="" class="w-75">
+      </div>
+      <div class="col-5">
+        <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam repellendus quos at. 
+          Nobis architecto voluptatem enim, amet labore ex dolor ut aut nihil. 
+          Aliquid nulla commodi perferendis ad! Rerum, aperiam!
+          </h3>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Header",
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
