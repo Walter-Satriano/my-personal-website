2d5e07315cada1476170cbf89c5496f265d615de
Walter <walter@quomi.it>
2022-01-21 12:14:36 +0100

    created logic to save languages in local storage and imported Montserrat google font

--- src/App.vue
+++ src/App.vue
@@ -15,6 +15,21 @@
 
   export default {
     name: "App",
+    data() {
+      return {
+        defaultLanguage: "en"
+      }
+    },
+    beforeMount() {
+      //get locale on page load
+      let lang = localStorage.getItem("lang") ? localStorage.getItem("lang") : this.defaultLanguage;
+      
+      //Set the locale property of the i18n object from either the localStorage variable or the defaultLanguage.
+      this.$i18n.locale = lang;
+
+      //Set the source language's file. We use setLocaleMessage() to load the corresponding JSON file.
+      this.$i18n.setLocaleMessage(lang, require(`./assets/lang/${lang}.json`));
+    },
     components: {
       NavbarDesktop,
       NavbarMobile,
@@ -25,5 +40,9 @@
 
 
 <style lang="scss" scoped>
+  @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
 
+  #app {
+    font-family: "Montserrat", sans-serif;
+  }
 </style>
