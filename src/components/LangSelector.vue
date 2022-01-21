f4f5916396d4eba4ea79a933f61624bdc943da98
Walter <walter@quomi.it>
2022-01-21 12:13:13 +0100

    create LangSelector component to manage the language change

---
+++ src/components/LangSelector.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="d-flex justify-content-center align-items-center text-uppercase">
+    <img src="../assets/images/gb.svg" alt="English-Flag">
+    <a :class="'link' + [activeLanguage === 'en' ? ' active' : '']"
+      @click="selectLanguage('en')">Eng
+    </a>
+
+    <span> | </span>
+    
+    <img src="../assets/images/it.svg" alt="Italian-Flag">
+    <a :class="'link' + [activeLanguage === 'it' ? ' active' : '']"
+      @click="selectLanguage('it')">Ita
+    </a>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "LangSelector",
+    data() {
+      return {
+        activeLanguage: this.$i18n.locale
+      }
+    },
+    methods: {
+      selectLanguage(lang) {
+
+        this.activeLanguage = lang; // update CSS class in selector
+        this.$i18n.locale = lang;
+        this.$i18n.setLocaleMessage(lang, require(`../assets/lang/${lang}.json`));
+
+        // persist selected language
+        localStorage.setItem("lang", lang);
+      },
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  img {
+    width: 25px;
+    height: auto;
+    margin-right: 0.6rem;
+  }
+
+  span {
+    font-size: 1.3rem;
+    font-weight: 500;
+    color: #D3D3D3;
+    margin: 0 1.5rem;
+  }
+
+  .link {
+    font-size: 1.3rem;
+    font-weight: 500;
+    color: #D3D3D3;
+    text-decoration: none;
+    cursor: pointer;
+
+    &.active {
+      color: #FFA500;
+    }
+  }
+</style>
