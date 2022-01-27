faadef8f0a2a354ab20c3ddd536d86a4bd30235e
Walter <walter@quomi.it>
2022-01-27 10:36:22 +0100

    created about section

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,22 @@
 <template>
-  <h2>THIS IS ABOUT PAGE</h2>
+  <section class="about_section bg-dark">
+    <h2 class="text-center text-uppercase py-3">{{ $t("aboutSection.title" ) }}</h2>
+
+    <div class="container-fluid">
+      <div class="row justify-content-center align-items-center">
+        <div class="col-12 col-lg-7">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Fuga porro sit facere quasi quia explicabo nobis unde vitae officia 
+            aperiam esse nesciunt vel, consectetur ducimus impedit, fugit aut cumque totam?
+          </p>
+        </div>
+        <div class="col-12 col-lg-5 text-center">
+          <img src="../assets/io.jpg" alt="" class="w-50">
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
@@ -10,5 +27,17 @@
 </script>
 
 <style lang="scss" scoped>
+  .about_section {
+    padding-bottom: 7rem;
+
+    h2 {
+      color: #FFA500;
+      font-weight: 600;
+    }
+
+    p {
+      color: #D3D3D3;
+    }
+  }
 
 </style>
