ad462321e7fe178fa77340d95d7a5a78fcbcb9d3
Walter <walter@quomi.it>
2022-01-31 18:41:14 +0100

    imported cookie component and styled it

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,20 @@
     <transition name="fade" mode="out-in">
       <router-view></router-view>
     </transition>
+
+    <cookie-law theme="myTheme" transitionName="fade">
+      <div slot-scope="props" class="container-fluid py-2">
+        <div class="row justify-content-center align-items-center">
+          <div class="col-12 col-md-8 text-center text-md-start">
+            <p class="m-0">{{ $t("cookies.text") }}</p>
+          </div>
+          <div class="col-12 col-md-4 text-center text-md-end pe-md-5">
+            <button class="btn btn-sm py-1 px-2 me-3" @click="props.accept">{{ $t("cookies.accept") }}</button>
+            <button class="btn btn-sm py-1 px-2" @click="props.close">{{ $t("cookies.decline") }}</button>
+          </div>
+        </div>
+      </div>
+    </cookie-law>
   </div>
 </template>
 
@@ -15,6 +29,7 @@
   import NavbarDesktop from "./components/NavbarDesktop.vue";
   import NavbarMobile from "./components/NavbarMobile.vue";
   import Header from "./components/Header.vue";
+  import CookieLaw from 'vue-cookie-law';
 
   export default {
     name: "App",
@@ -36,7 +51,8 @@
     components: {
       NavbarDesktop,
       NavbarMobile,
-      Header
+      Header,
+      CookieLaw
     },
   }
 </script>
@@ -63,4 +79,88 @@
       opacity: 0;
     }
   }
+
+  .Cookie > * {
+    margin: 0;
+  }
+
+  .Cookie--myTheme {
+    background-color: #D3D3D3;
+    font-weight: 600;
+    opacity: 0.95;
+
+    p {
+      color: #212529;
+      font-size: 0.7rem;
+    }
+
+    button {
+      background-color: #FFA500;
+      border: 2px solid #212529;
+      color: #212529;
+      font-size: 0.7rem;
+      font-weight: 600;
+
+      &:hover {
+        background-color: #212529;
+        color: #FFA500;
+      }
+    }
+  }
+
+  @media (min-width: 576px) {
+    .Cookie--myTheme {
+
+      p {
+        font-size: 0.8rem;
+        padding-bottom: 0.5rem;
+      }
+
+      button {
+        font-size: 0.8rem;
+      }
+    }    
+  }
+
+  @media (min-width: 768px) {
+    .Cookie--myTheme {
+
+      p {
+        font-size: 0.9rem;
+        padding-bottom: 0;
+      }
+
+      button {
+        font-size: 0.9rem;
+      }
+    }    
+  }
+
+  @media (min-width: 992px) {
+    .Cookie--myTheme {
+
+      p {
+        font-size: 1rem;
+        padding-bottom: 0;
+      }
+
+      button {
+        font-size: 1rem;
+      }
+    }    
+  }
+
+  @media (min-width: 1200px) {
+    .Cookie--myTheme {
+
+      p {
+        font-size: 1.1rem;
+        padding-bottom: 0;
+      }
+
+      button {
+        font-size: 1.1rem;
+      }
+    }    
+  }  
 </style>
